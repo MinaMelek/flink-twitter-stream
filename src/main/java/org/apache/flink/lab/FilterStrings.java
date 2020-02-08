751a95ea68e056fcd1d17be517b84c5be788305a
MinaMelek <m.melek@nu.edu.eg>
2020-02-09 00:00:56 +0200

    source edit

--- src/main/java/org/apache/flink/lab/FilterStrings.java
+++ src/main/java/org/apache/flink/lab/FilterStrings.java
@@ -1,46 +1,46 @@
-// package org.apache.flink.lab;
+package org.apache.flink.lab;
 
-// import org.apache.flink.api.common.functions.FlatMapFunction;
-// import org.apache.flink.api.java.tuple.Tuple2;
-// import org.apache.flink.streaming.api.datastream.DataStream;
-// import org.apache.flink.streaming.api.datastream.IterativeStream;
-// import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
-// import org.apache.flink.streaming.api.windowing.time.Time;
-// import org.apache.flink.streaming.api.windowing.assigners.TumblingProcessingTimeWindows;
-// import org.apache.flink.util.Collector;
-// import java.util.Arrays;
-// import java.util.List;
+import org.apache.flink.api.common.functions.FlatMapFunction;
+import org.apache.flink.api.java.tuple.Tuple2;
+import org.apache.flink.streaming.api.datastream.DataStream;
+import org.apache.flink.streaming.api.datastream.IterativeStream;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.api.windowing.time.Time;
+import org.apache.flink.streaming.api.windowing.assigners.TumblingProcessingTimeWindows;
+import org.apache.flink.util.Collector;
+import java.util.Arrays;
+import java.util.List;
 
 
-// public class FilterStrings {
-//     public static String[] swords = new String[]{"some", "for", "do", "its", "yours", "such", "into", "of", "most", "itself", "other", "off", "is", "s", "am", "or", "who", "as", "from", "him", "each"};
-//     public static List<String> SListwords = Arrays.asList(swords);
-//     public static void main(String[] args) throws Exception {
+public class FilterStrings {
+    public static String[] swords = new String[]{"some", "for", "do", "its", "yours", "such", "into", "of", "most", "itself", "other", "off", "is", "s", "am", "or", "who", "as", "from", "him", "each"};
+    public static List<String> SListwords = Arrays.asList(swords);
+    public static void main(String[] args) throws Exception {
 
-//         StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
         
-//         DataStream<Tuple2<String, Integer>> dataStream = env
-//                 .socketTextStream("localhost", 5567)
-//                 .flatMap(new Splitter())
-//                 .filter((x)-> !SListwords.contains(x.f0) )
-//                 .keyBy(0)
-//                 .sum(1);
+        DataStream<Tuple2<String, Integer>> dataStream = env
+                .socketTextStream("localhost", 5567)
+                .flatMap(new Splitter())
+                .filter((x)-> !SListwords.contains(x.f0) )
+                .keyBy(0)
+                .sum(1);
 
-//         // IterativeStream<Tuple2<String, Integer>> iteration = dataStream.iterate();
-//         // DataStream<Tuple2<String, Integer>> iterationBody = iteration.map((x) -> x);
-//         // iteration.closeWith(iterationBody.filter((x)-> !SListwords.contains(x)));
-//         // DataStream<Tuple2<String, Integer>> output = iterationBody.keyBy(0).sum(1);
-//         dataStream.print();
+        // IterativeStream<Tuple2<String, Integer>> iteration = dataStream.iterate();
+        // DataStream<Tuple2<String, Integer>> iterationBody = iteration.map((x) -> x);
+        // iteration.closeWith(iterationBody.filter((x)-> !SListwords.contains(x)));
+        // DataStream<Tuple2<String, Integer>> output = iterationBody.keyBy(0).sum(1);
+        dataStream.print();
 
-//         env.execute("FilterStrings");
-//     }
+        env.execute("FilterStrings");
+    }
 
-//     public static class Splitter implements FlatMapFunction<String, Tuple2<String, Integer>> {
-//         @Override
-//         public void flatMap(String sentence, Collector<Tuple2<String, Integer>> out) throws Exception {
-//             for (String word: sentence.split(" ")) {
-//                 out.collect(new Tuple2<String, Integer>(word, 1));
-//             }
-//         }
-//     }
-// }
+    public static class Splitter implements FlatMapFunction<String, Tuple2<String, Integer>> {
+        @Override
+        public void flatMap(String sentence, Collector<Tuple2<String, Integer>> out) throws Exception {
+            for (String word: sentence.split(" ")) {
+                out.collect(new Tuple2<String, Integer>(word, 1));
+            }
+        }
+    }
+}
--- src/main/java/org/apache/flink/lab/TumblingWindowT4.java
+++ src/main/java/org/apache/flink/lab/TumblingWindowT4.java
@@ -1,87 +1,87 @@
-// package org.apache.flink.lab;
-// import org.apache.flink.api.common.functions.MapFunction;
-// import org.apache.flink.api.common.functions.ReduceFunction;
-// import org.apache.flink.api.java.tuple.Tuple2;
-// import org.apache.flink.api.java.tuple.Tuple3;
-// import org.apache.flink.api.java.utils.ParameterTool;
-// import org.apache.flink.streaming.api.datastream.DataStream;
-// import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
-// import org.apache.flink.streaming.api.windowing.time.Time;
-// import org.apache.flink.streaming.api.windowing.assigners.TumblingProcessingTimeWindows;
-// public class TumblingWindowT4 {
+package org.apache.flink.lab;
+import org.apache.flink.api.common.functions.MapFunction;
+import org.apache.flink.api.common.functions.ReduceFunction;
+import org.apache.flink.api.java.tuple.Tuple2;
+import org.apache.flink.api.java.tuple.Tuple3;
+import org.apache.flink.api.java.utils.ParameterTool;
+import org.apache.flink.streaming.api.datastream.DataStream;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.api.windowing.time.Time;
+import org.apache.flink.streaming.api.windowing.assigners.TumblingProcessingTimeWindows;
+public class TumblingWindowT4 {
 
-//     public static void main(String[] args) throws Exception {
-//         final ParameterTool params = ParameterTool.fromArgs(args);
+    public static void main(String[] args) throws Exception {
+        final ParameterTool params = ParameterTool.fromArgs(args);
 
-//         final StreamExecutionEnvironment env =
-//                 StreamExecutionEnvironment.getExecutionEnvironment();
+        final StreamExecutionEnvironment env =
+                StreamExecutionEnvironment.getExecutionEnvironment();
 
-//         env.getConfig().setGlobalJobParameters(params);
+        env.getConfig().setGlobalJobParameters(params);
 
-//         DataStream<String> dataStream = StreamUtil.getDataStream(env, params);
+        DataStream<String> dataStream = StreamUtil.getDataStream(env, params);
 
-//         if (dataStream == null) {
-//             System.exit(1);
-//             return;
-//         }
+        if (dataStream == null) {
+            System.exit(1);
+            return;
+        }
 
-//         DataStream<Tuple2<String, Double>> SumViewStream = dataStream
-//                 .map(new RowSplitter())
-//                 .keyBy(0)
-//                 .window(TumblingProcessingTimeWindows.of(Time.seconds(30)))
-//                 .reduce(new Sum())
-//                 .map(new Converter());
+        DataStream<Tuple2<String, Double>> SumViewStream = dataStream
+                .map(new RowSplitter())
+                .keyBy(0)
+                .window(TumblingProcessingTimeWindows.of(Time.seconds(30)))
+                .reduce(new Sum())
+                .map(new Converter());
 
-//         SumViewStream.print();
+        SumViewStream.print();
 
-//         env.execute("Tumbling Window average");
-//     }
-
-
-//     public static class RowSplitter implements
-//             MapFunction<String, Tuple3<String, Double, Double>> {
-
-//         public Tuple3<String, Double, Double> map(String row)
-//                 throws Exception {
-//             String[] fields = row.split(" ");
-//             if (fields.length == 2) {
-//                 return new Tuple3<String, Double, Double>(
-//                         fields[0] /* webpage id */,
-//                         Double.parseDouble(fields[1]) /* view time in minutes */,
-//                         1.0);
-//             }
+        env.execute("Tumbling Window average");
+    }
 
 
-//             return null;
-//         }
-//     }
+    public static class RowSplitter implements
+            MapFunction<String, Tuple3<String, Double, Double>> {
 
-//     public static class Converter implements
-//             MapFunction<Tuple3<String, Double, Double>, Tuple2<String, Double>> {
-
-//         public Tuple2<String, Double> map(Tuple3<String, Double, Double> val)
-//                 throws Exception {
-//             return new Tuple2<String, Double>(
-//                     val.f0/* webpage id */,
-//                     val.f1 / val.f2 /* avg time */);
-//         }
-//     }
-
-//     public static class Sum implements
-//             ReduceFunction<Tuple3<String, Double, Double>> {
-
-//         public Tuple3<String, Double, Double> reduce(
-//                 Tuple3<String, Double, Double> cumulative,
-//                 Tuple3<String, Double, Double> input) {
-
-//             return new Tuple3<String, Double, Double>(
-//                     input.f0,
-//                     cumulative.f1 + input.f1,
-//                     cumulative.f2 + input.f2
-//                     );
-//         }
-//     }
+        public Tuple3<String, Double, Double> map(String row)
+                throws Exception {
+            String[] fields = row.split(" ");
+            if (fields.length == 2) {
+                return new Tuple3<String, Double, Double>(
+                        fields[0] /* webpage id */,
+                        Double.parseDouble(fields[1]) /* view time in minutes */,
+                        1.0);
+            }
 
 
+            return null;
+        }
+    }
 
-// }
+    public static class Converter implements
+            MapFunction<Tuple3<String, Double, Double>, Tuple2<String, Double>> {
+
+        public Tuple2<String, Double> map(Tuple3<String, Double, Double> val)
+                throws Exception {
+            return new Tuple2<String, Double>(
+                    val.f0/* webpage id */,
+                    val.f1 / val.f2 /* avg time */);
+        }
+    }
+
+    public static class Sum implements
+            ReduceFunction<Tuple3<String, Double, Double>> {
+
+        public Tuple3<String, Double, Double> reduce(
+                Tuple3<String, Double, Double> cumulative,
+                Tuple3<String, Double, Double> input) {
+
+            return new Tuple3<String, Double, Double>(
+                    input.f0,
+                    cumulative.f1 + input.f1,
+                    cumulative.f2 + input.f2
+                    );
+        }
+    }
+
+
+
+}
